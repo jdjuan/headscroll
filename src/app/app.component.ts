acb710deb8769143d8ddf1ff3ad0962d27981454
Juan Herrera <jherrera@parkside.at>
2020-12-06 16:08:46 +0100

    refactor: code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import * as tmPose from '@teachablemachine/pose';
 import { Keypoint } from '@tensorflow-models/posenet';
 
@@ -14,52 +15,45 @@ export enum Classes {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'scroll-wtih-head';
+  @ViewChild('canvas') canvas: ElementRef;
+  @ViewChild('iframeWrapper') iframeWrapper: ElementRef;
+  readonly MODEL_URL = 'https://teachablemachine.withgoogle.com/models/l5fbLAKJu/';
+  readonly CAMERA_SIZE = 300;
+  readonly SCROLL_SPEED = 7;
+  readonly FORECAST_CONFIDENCE = 0.95;
+  readonly SCROLL_BUFFER = 1.5;
   model: tmPose.CustomPoseNet;
-  webcam: tmPose.Webcam;
   ctx: CanvasRenderingContext2D;
-  labelContainer: HTMLElement;
   maxPredictions: number;
-  size = 600;
-  url = 'https://teachablemachine.withgoogle.com/models/l5fbLAKJu/';
+  webcam: tmPose.Webcam;
   forecast: Classes;
-  classes = Classes;
+  source: SafeResourceUrl;
+  iframeHeight = 100; // TODO: Calculate screen height
 
-  constructor() {
+  constructor(sanitizer: DomSanitizer) {
     // More API functions here:
     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
-
-    // the link to your model provided by Teachable Machine export panel
-    this.init();
+    this.initCanvas();
+    this.source = sanitizer.bypassSecurityTrustResourceUrl(
+      'https://tabs.ultimate-guitar.com/tab/foo-fighters/times-like-these-chords-1211863'
+    );
   }
 
-  scrollDown() {
-    document.getElementById('iframe-wrapper').scrollBy(0, 7);
-  }
-  scrollUp() {
-    document.getElementById('iframe-wrapper').scrollBy(0, -7);
-  }
-
-  async init() {
-    const modelURL = this.url + 'model.json';
-    const metadataURL = this.url + 'metadata.json';
+  async initCanvas(): Promise<void> {
+    const modelURL = this.MODEL_URL + 'model.json';
+    const metadataURL = this.MODEL_URL + 'metadata.json';
     this.model = await tmPose.load(modelURL, metadataURL);
     this.maxPredictions = this.model.getTotalClasses();
     this.setupWebCam();
-    // append/get elements to the DOM
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    canvas.width = this.size;
-    canvas.height = this.size;
+    const canvas = this.canvas.nativeElement as HTMLCanvasElement;
+    canvas.width = this.CAMERA_SIZE;
+    canvas.height = this.CAMERA_SIZE;
     this.ctx = canvas.getContext('2d');
-    this.labelContainer = document.getElementById('label-container');
-    for (let i = 0; i < this.maxPredictions; i++) {
-      this.labelContainer.appendChild(document.createElement('div'));
-    }
   }
 
-  async setupWebCam() {
-    const flip = true; // whether to flip the webcam
-    this.webcam = new tmPose.Webcam(this.size, this.size, flip); // width, height, flip
+  async setupWebCam(): Promise<void> {
+    const flip = true;
+    this.webcam = new tmPose.Webcam(this.CAMERA_SIZE, this.CAMERA_SIZE, flip);
     await this.webcam.setup(); // request access to the webcam
     await this.webcam.play();
     window.requestAnimationFrame(this.loop);
@@ -71,27 +65,20 @@ export class AppComponent {
     window.requestAnimationFrame(this.loop);
   }
 
-  async predict() {
-    const { pose, posenetOutput } = await this.model.estimatePose(
-      this.webcam.canvas
-    );
-    const prediction = await this.model.predict(posenetOutput);
-    this.mapPrediction(prediction);
+  async predict(): Promise<void> {
+    const { pose, posenetOutput } = await this.model.estimatePose(this.webcam.canvas);
+    const output = await this.model.predict(posenetOutput);
+    this.getForecast(output);
     this.drawPose(pose);
   }
 
-  mapPrediction(
-    groups: {
-      className: string;
-      probability: number;
-    }[]
-  ): void {
-    const left = groups.find((group) => group.className === Classes.Left);
-    const right = groups.find((group) => group.className === Classes.Right);
-    if (left.probability > 0.95) {
+  getForecast(output: { className: string; probability: number }[]): void {
+    const leftForecast = output.find((entry) => entry.className === Classes.Left);
+    const rightForecast = output.find((entry) => entry.className === Classes.Right);
+    if (leftForecast.probability > this.FORECAST_CONFIDENCE) {
       this.forecast = Classes.Left;
       this.scrollDown();
-    } else if (right.probability > 0.95) {
+    } else if (rightForecast.probability > this.FORECAST_CONFIDENCE) {
       this.scrollUp();
       this.forecast = Classes.Right;
     } else {
@@ -99,10 +86,28 @@ export class AppComponent {
     }
   }
 
-  drawPose(pose: { keypoints: Keypoint[] }) {
+  scrollDown(): void {
+    const { scrollTop, scrollHeight, clientHeight } = this.iframeWrapper.nativeElement;
+    const height = scrollHeight - clientHeight;
+    if (height === scrollTop) {
+      this.iframeHeight *= this.SCROLL_BUFFER;
+    }
+    this.iframeWrapper.nativeElement.scrollBy(0, this.SCROLL_SPEED);
+  }
+  scrollUp(): void {
+    this.iframeWrapper.nativeElement.scrollBy(0, -this.SCROLL_SPEED);
+  }
+
+  hasTiltedLeft(): boolean {
+    return this.forecast === Classes.Left;
+  }
+  hasTiltedRight(): boolean {
+    return this.forecast === Classes.Right;
+  }
+
+  drawPose(pose: { keypoints: Keypoint[] }): void {
     if (this.webcam.canvas) {
       this.ctx.drawImage(this.webcam.canvas, 0, 0);
-      // draw the keypoints and skeleton
       if (pose) {
         const minPartConfidence = 0.5;
         tmPose.drawKeypoints(pose.keypoints, minPartConfidence, this.ctx);
