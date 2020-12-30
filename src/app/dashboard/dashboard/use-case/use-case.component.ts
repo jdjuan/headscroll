9ca8d200a6b6eafa2d0c3e059d74f20d50b86acc
Juan Herrera <herrera@parkside.at>
2020-12-30 05:37:28 +0100

    feat: add slide 2

--- src/app/dashboard/dashboard/use-case/use-case.component.ts
+++ src/app/dashboard/dashboard/use-case/use-case.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { concat, from, of, timer } from 'rxjs';
+import { concatMap, delay, exhaustMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-use-case',
   templateUrl: './use-case.component.html',
   styleUrls: ['./use-case.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UseCaseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  instruments = ['guitar', 'piano', 'ukelele', 'accordion', 'harmonica'];
+  instrument: string;
+  readonly WRITE_SPEED = 200;
+  readonly ERASE_SPEED = 150;
+  readonly CHANGE_SPEED = 5000;
+  constructor() {
+    this.instrument = this.instruments.shift();
+    this.instruments.push(this.instrument);
   }
 
+  ngOnInit(): void {
+    timer(this.CHANGE_SPEED, this.CHANGE_SPEED)
+      .pipe(
+        exhaustMap(() => {
+          const nextInstrument = this.instruments.shift();
+          this.instruments.push(nextInstrument);
+          return concat(this.erase$(this.instrument), this.write$(nextInstrument));
+        })
+      )
+      .subscribe();
+  }
+
+  erase$ = (instrument: string) => {
+    return from(instrument).pipe(
+      concatMap((letter) => of(letter).pipe(delay(this.ERASE_SPEED))),
+      tap(() => (this.instrument = this.instrument.slice(0, -1)))
+    );
+  }
+
+  write$ = (instrument: any) => {
+    return from(instrument).pipe(
+      concatMap((letter) => of(letter).pipe(delay(this.WRITE_SPEED))),
+      tap((letter) => (this.instrument += letter))
+    );
+  }
 }
