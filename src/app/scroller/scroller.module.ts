ab6b3204cd5d2c05aedd0312dcde589825bda813
Juan Herrera <herrera@parkside.at>
2021-01-06 14:25:49 +0100

    refactor: remove material

--- src/app/scroller/scroller.module.ts
+++ src/app/scroller/scroller.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollerRoutingModule } from './scroller-routing.module';
 import { ScrollerComponent } from './scroller/scroller.component';
-import { MaterialModule } from '../material/material.module';
-import { SidenavComponent } from './scroller/sidenav/sidenav.component';
 import { CameraComponent } from './scroller/camera/camera.component';
 
 @NgModule({
-  declarations: [ScrollerComponent, SidenavComponent, CameraComponent],
+  declarations: [ScrollerComponent, CameraComponent],
   imports: [
     CommonModule,
     ScrollerRoutingModule,
-    MaterialModule
   ]
 })
 export class ScrollerModule { }
