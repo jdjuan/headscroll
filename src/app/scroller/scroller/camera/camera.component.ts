d5f484d3159d3d32b470cc86e5a6babd9cec526a
Juan Herrera <herrera@parkside.at>
2021-01-30 15:25:06 +0100

    refactor: cleanup camera component

--- src/app/scroller/scroller/camera/camera.component.ts
+++ src/app/scroller/scroller/camera/camera.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
 import { Webcam, CustomPoseNet, load } from '@teachablemachine/pose';
 import { CameraService } from '../../services/camera.service';
 import { UntilDestroy } from '@ngneat/until-destroy';
@@ -6,12 +6,7 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { LARGE_BREAKPOINT } from 'src/app/core/constants';
 import { ConfigService } from '../../services/config.service';
 import { timer } from 'rxjs';
-
-export enum Classes {
-  Left = 'Left',
-  Right = 'Right',
-  Neutral = 'Neutral',
-}
+import { filter } from 'rxjs/operators';
 
 @UntilDestroy()
 @Component({
@@ -19,55 +14,37 @@ export enum Classes {
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.scss'],
 })
-export class CameraComponent implements OnInit {
-  readonly MODEL_URL = 'assets/model.json';
-  // readonly MODEL_URL = 'https://teachablemachine.withgoogle.com/models/l5fbLAKJu/model.json';
-  readonly METADATA_URL = 'assets/metadata.json';
-  // readonly METADATA_URL = 'https://teachablemachine.withgoogle.com/models/l5fbLAKJu/metadata.json';
-  readonly DEFAULT_CAMERA_SIZE = 400;
-  readonly SMALL_CAMERA_SIZE = 100;
-  readonly FORECAST_CONFIDENCE = 0.95;
-  readonly DEBOUNCE_PREDICTION_TIME = 100;
-
-  @Output() scrolling = new EventEmitter<boolean>();
+export class CameraComponent {
   @ViewChild('video') video: ElementRef<HTMLVideoElement>;
-  cameraSize = this.DEFAULT_CAMERA_SIZE;
-  isLoadingCamera = true;
-  isMobile: boolean;
-  webcam: Webcam;
+  @Output() scrolling = new EventEmitter<boolean>();
+  readonly DEBOUNCE_PREDICTION_TIME = 100;
   model: CustomPoseNet;
-  ctx: CanvasRenderingContext2D;
 
   constructor(private breakpointObserver: BreakpointObserver, private cameraService: CameraService, private configService: ConfigService) {
-    this.isMobile = this.breakpointObserver.isMatched(LARGE_BREAKPOINT);
-    this.cameraSize = this.isMobile ? this.SMALL_CAMERA_SIZE : this.cameraSize;
-    this.cameraService.selectedCamera$.subscribe((deviceId) => {
-      if (deviceId) {
-        this.setupWebCam(deviceId);
-      }
-    });
-  }
-  ngOnInit(): void {
+    this.cameraService.selectedCamera$.pipe(filter((deviceId) => !!deviceId)).subscribe(this.setupWebCam);
     this.init();
   }
 
   async init(): Promise<void> {
+    const [MODEL_URL, METADATA_URL] = ['assets/model.json', 'assets/metadata.json'];
     const cameras = await this.cameraService.getAvailableCameras();
     if (cameras) {
       const [firstCamera] = cameras;
       const { deviceId } = firstCamera;
-      this.model = await load(this.MODEL_URL, this.METADATA_URL);
+      this.model = await load(MODEL_URL, METADATA_URL);
       this.setupWebCam(deviceId);
     }
   }
 
-  async setupWebCam(deviceId: string): Promise<void> {
-    const flip = true;
-    this.webcam = new Webcam(this.cameraSize, this.cameraSize, flip);
+  setupWebCam = async (deviceId: string): Promise<void> => {
     try {
-      await this.webcam.setup({ deviceId });
-      this.isLoadingCamera = false;
-      this.video.nativeElement.srcObject = this.webcam.webcam.srcObject;
+      const isMobile = this.breakpointObserver.isMatched(LARGE_BREAKPOINT);
+      const DEFAULT_CAMERA_SIZE = 400;
+      const SMALL_CAMERA_SIZE = 100;
+      const cameraSize = isMobile ? SMALL_CAMERA_SIZE : DEFAULT_CAMERA_SIZE;
+      const webcam = new Webcam(cameraSize, cameraSize);
+      await webcam.setup({ deviceId });
+      this.video.nativeElement.srcObject = webcam.webcam.srcObject;
       this.video.nativeElement.play();
     } catch (error) {
       console.log('Could not load the camera');
@@ -87,11 +64,13 @@ export class CameraComponent implements OnInit {
   }
 
   forecast(output: { className: string; probability: number }[]): void {
-    const leftForecast = output.find((entry) => entry.className === Classes.Left);
-    const rightForecast = output.find((entry) => entry.className === Classes.Right);
-    if (leftForecast.probability > this.FORECAST_CONFIDENCE) {
+    const [LEFT, RIGHT] = ['Left', 'Right'];
+    const FORECAST_CONFIDENCE = 0.95;
+    const leftForecast = output.find((entry) => entry.className === LEFT);
+    const rightForecast = output.find((entry) => entry.className === RIGHT);
+    if (leftForecast.probability > FORECAST_CONFIDENCE) {
       this.scrolling.emit(false);
-    } else if (rightForecast.probability > this.FORECAST_CONFIDENCE) {
+    } else if (rightForecast.probability > FORECAST_CONFIDENCE) {
       this.scrolling.emit(true);
     }
   }
